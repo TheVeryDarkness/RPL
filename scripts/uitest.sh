31504c3ecd2cb4bec78dd46dc5532351a9c6de42
TheVeryDarkness <3266343194@qq.com>
2025-06-25 00:48:24 +0800

    Update scripts

--- scripts/uitest.sh
+++ scripts/uitest.sh
@@ -5,7 +5,18 @@ set -eux
 export RUSTC_ICE=0
 export RUST_BACKTRACE=1
 export RUSTC_LOG_COLOR=always
-# export RUSTC_LOG="rpl=info"
+# export RUSTC_LOG="rpl=trace"
 export RPL_PATS="$1"
 
-cargo uitest -- "$2"
+case ${3:-'--test'} in
+    --test)
+        cargo uitest -- "${2:-}"
+        ;;
+    --bless)
+        cargo uibless -- "${2:-}"
+        ;;
+    *)
+        echo "Invalid mode."
+        exit -1
+        ;;
+esac
