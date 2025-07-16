f589616965d2eb59720533430a81b28cb7e8c02a
TheVeryDarkness <3266343194@qq.com>
2025-07-16 17:22:09 +0800

    Create timing.sh for measuring performance

---
+++ scripts/timing.sh
@@ -0,0 +1,25 @@
+clear
+
+set -eux
+
+export RUSTC_ICE=0
+export RUST_BACKTRACE=1
+RPL_ROOT="$(realpath $(realpath $0)/../..)"
+
+cd $RPL_ROOT
+
+export RPL_PATS="$(realpath $RPL_ROOT/docs/patterns-pest/clippy)"
+
+for i in {1..5}
+do
+    cargo lintcheck --timing -j 24 --crates-toml crates/lintcheck/1000.toml
+    cp ./lintcheck-logs/1000_logs.txt ./lintcheck-logs/1000_logs.clippy.$i.txt
+done
+
+export RPL_PATS="$(realpath $RPL_ROOT/docs/patterns-pest)"
+
+for i in {1..5}
+do
+    cargo lintcheck --timing -j 24 --crates-toml crates/lintcheck/1000.toml
+    cp ./lintcheck-logs/1000_logs.txt ./lintcheck-logs/1000_logs.all.$i.txt
+done
