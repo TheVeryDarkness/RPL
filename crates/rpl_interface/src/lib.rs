bd9454f48c2c8f1398f8842b64357c70d0bcadbb
TheVeryDarkness <2301213215@stu.pku.edu.cn>
2026-04-22 14:02:45 +0800

    Track RPL_PATS environment variable and pattern files (#110)
    * Track RPL_PATS environment variable and pattern files
    
    * Avoid hard-coding RPL_PATS_ENV to "PRL_PATS"

--- crates/rpl_interface/src/lib.rs
+++ crates/rpl_interface/src/lib.rs
@@ -18,7 +18,7 @@ extern crate rustc_session;
 extern crate rustc_span;
 
 mod callbacks;
-pub use callbacks::{DefaultCallbacks, RPL_ARGS_ENV, RplCallbacks, RustcCallbacks};
+pub use callbacks::{DefaultCallbacks, RPL_ARGS_ENV, RPL_PATS_ENV, RplCallbacks, RustcCallbacks};
 
 static RPL_LOCALE_RESOURCES: &[&str] = &[
     rpl_driver::DEFAULT_LOCALE_RESOURCE,
